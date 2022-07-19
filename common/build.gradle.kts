fa18529500711935d9cd5b1508cc705af07085e7
Dominik Rzepka <dominik.1.rzepka@gmail.com>
2022-07-19 14:59:57 +0200

    That dependency was needed after all

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("com.github.gmazzo.buildconfig") version "3.1.0"
 }
 
+val kotlinVersion: String by System.getProperties()
 val kotlinxSerializationVersion: String by System.getProperties()
 val ktorVersion: String by System.getProperties()
 val coroutinesVersion: String by System.getProperties()
@@ -21,6 +22,7 @@ kotlin {
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:$kotlinxSerializationVersion")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
                 implementation("io.github.microutils:kotlin-logging:2.1.23")
+                implementation("org.jetbrains.kotlin:atomicfu:$kotlinVersion") // https://github.com/Kotlin/kotlinx.coroutines/issues/3305#issuecomment-1141239424
                 api("org.jetbrains.kotlinx:kotlinx-datetime:0.3.2")
                 compileOnly("io.ktor:ktor-client-core:$ktorVersion")
             }
