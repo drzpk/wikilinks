689416aa19992ef34fb8baa012e114fe24d3ed3a
Dominik Rzepka <dominik.1.rzepka@gmail.com>
2022-08-01 14:50:17 +0200

    Fix the SQLite error when using too many query parameters

--- backend/src/commonMain/kotlin/dev/drzepka/wikilinks/app/db/DbLinksRepository.kt
+++ backend/src/commonMain/kotlin/dev/drzepka/wikilinks/app/db/DbLinksRepository.kt
@@ -2,29 +2,50 @@ package dev.drzepka.wikilinks.app.db
 
 import dev.drzepka.wikilinks.app.model.Link
 import dev.drzepka.wikilinks.db.links.LinksDatabase
+import kotlin.math.ceil
 
 class DbLinksRepository(private val database: LinksDatabase) : LinksRepository {
 
     override fun getInLinksCount(vararg pageIds: Int): Int {
-        val ids = pageIds.map{it.toLong()}
-        return database.linksQueries.countInLinks(ids).executeAsOneOrNull()?.SUM?.toInt() ?: 0
+        return splitQuery(pageIds) {
+            database.linksQueries.countInLinks(it).executeAsOneOrNull()?.SUM?.toInt() ?: 0
+        }.sum()
     }
 
     override fun getOutLinksCount(vararg pageIds: Int): Int {
-        val ids = pageIds.map{it.toLong()}
-        return database.linksQueries.countOutLinks(ids).executeAsOneOrNull()?.SUM?.toInt() ?: 0
+        return splitQuery(pageIds) {
+            database.linksQueries.countOutLinks(it).executeAsOneOrNull()?.SUM?.toInt() ?: 0
+        }.sum()
     }
 
     override fun getInLinks(vararg pageIds: Int): List<Link> {
-        val ids = pageIds.map { it.toLong() }
-        val rows = database.linksQueries.getInLinks(ids).executeAsList()
-        return rows.flatMap { splitInLinks(it.in_links, it.page_id.toInt()) }
+        return splitQuery(pageIds) { database.linksQueries.getInLinks(it).executeAsList() }
+            .flatMap { it }
+            .flatMap { splitInLinks(it.in_links, it.page_id.toInt()) }
+            .toList()
     }
 
     override fun getOutLinks(vararg pageIds: Int): List<Link> {
-        val ids = pageIds.map { it.toLong() }
-        val rows = database.linksQueries.getOutLinks(ids).executeAsList()
-        return rows.flatMap { splitOutLinks(it.page_id.toInt(), it.out_links) }
+        return splitQuery(pageIds) { database.linksQueries.getOutLinks(it).executeAsList() }
+            .flatMap { it }
+            .flatMap { splitOutLinks(it.page_id.toInt(), it.out_links) }
+            .toList()
+    }
+
+    private fun <T> splitQuery(values: IntArray, handler: (section: List<Long>) -> T): Sequence<T> {
+        val longValues = values.map { it.toLong() }
+        return if (longValues.size <= PARAMETER_SIZE_LIMIT) {
+            sequenceOf(handler.invoke(longValues))
+        } else {
+            val sectionCount = ceil(longValues.size.toDouble() / PARAMETER_SIZE_LIMIT).toInt()
+            return (0 until sectionCount)
+                .asSequence()
+                .map { sectionNo ->
+                    val endIndex = ((sectionNo + 1) * PARAMETER_SIZE_LIMIT).coerceAtMost(longValues.size)
+                    val section = longValues.subList(sectionNo * PARAMETER_SIZE_LIMIT, endIndex)
+                    handler.invoke(section)
+                }
+        }
     }
 
     private fun splitInLinks(raw: String, to: Int): List<Link> = splitLinks(raw).map { Link(it, to) }
@@ -36,5 +57,10 @@ class DbLinksRepository(private val database: LinksDatabase) : LinksRepository {
             return emptyList()
 
         return raw.split(',').map { it.toInt() }
+    }
+
+    companion object {
+        // https://www.sqlite.org/limits.html, 9th limit
+        private const val PARAMETER_SIZE_LIMIT = 32766
     }
 }
